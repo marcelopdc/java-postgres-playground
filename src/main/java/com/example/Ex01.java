4e40bba606141bc6fe9fb92485d9f9baac769431
Marcelo Carvalho <marcelopcs@gmail.com>
2023-08-21 14:28:55 +0000

    Commit01

---
+++ src/main/java/com/example/Ex01.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Ex01 {
+   public static void main(String[] args) {
+
+    int idade = 22;
+    int peso = 50; 
+    char sexo = 'M';
+    boolean nessecidadesEspeciais = false;
+    System.out.println("\nA idade é :"+ idade + " e o peso é: " + peso);
+    System.out.println("\nO seu sexo é: "+sexo);
+    System.out.println("\nTem necessidades especiais: "+nessecidadesEspeciais);
+    System.out.println("\n");
+
+   }
+}
